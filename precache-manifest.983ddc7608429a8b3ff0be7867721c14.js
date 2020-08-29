e9a864c465399399de171d40f6c9bccd6363e4bd
Cailyn Edwards <cailynedwards@MacBook-Pro.localdomain>
2020-08-29 12:12:43 -0400

    Updates

---
+++ precache-manifest.983ddc7608429a8b3ff0be7867721c14.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65c93ab0ac5755a33f0a7b7fb175ecfc",
+    "url": "/portfolio2020/index.html"
+  },
+  {
+    "revision": "71e5b7c925e9330bb14c",
+    "url": "/portfolio2020/static/css/main.2b2a5b8c.chunk.css"
+  },
+  {
+    "revision": "66adf6a79df3e5077826",
+    "url": "/portfolio2020/static/js/2.422f6ae6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio2020/static/js/2.422f6ae6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71e5b7c925e9330bb14c",
+    "url": "/portfolio2020/static/js/main.02fe3795.chunk.js"
+  },
+  {
+    "revision": "09334edcd1c725506272",
+    "url": "/portfolio2020/static/js/runtime-main.a7380557.js"
+  }
+]);
